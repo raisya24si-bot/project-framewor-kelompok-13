99b31369d0cbd19e789a927f81fac1784ea0baad
dane-09 <zidane23ti@mahasiswa.pcr.ac.id>
2025-10-01 10:42:05 +0700

    ikbar done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
